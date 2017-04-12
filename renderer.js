cbdd6779a208d89142bac7d7ec733a81b2dedf40
Stef Velzel <stefvelzel@hotmail.com>
2017-04-12 17:44:24 +0200

    Support function & mouse dragging of objects

--- renderer.js
+++ renderer.js
@@ -5,7 +5,43 @@
 		scene: undefined,
 		camera: undefined,
 		raycaster: undefined,
+		intersect: undefined, // Undefined if mouse not intersecting with object
+		dragging: false
 	};
+
+	///////////////////////////////////
+	// GJK algorithm
+	///////////////////////////////////
+	function support(shape1, shape2, d) // vertices of shape 1 and 2 and direction
+	{
+		// Get some point on the minkowski sum (difference really)
+		// Do this by getting the farthest point in both directions of d
+		var dir = d.clone().normalize();
+
+		var p1 = getFarthestPoint(shape1, dir);
+		var p2 = getFarthestPoint(shape2, dir.negate());
+
+		return p1.clone().sub(p2);
+	}
+
+	function getFarthestPoint(shape, d) // vertices of shape and direction (normalized)
+	{
+		// Project all vertices onto shape and get the longest
+		var p = new THREE.Vector3(0, 0, 0);
+		var l = 0;
+
+		for(var i = 0; i < shape.length; ++i)
+		{
+			var proj = shape[i].dot(d);
+			if(proj > l)
+			{
+				p = shape[i];
+				l = proj;
+			}
+		}
+
+		return p;
+	}
 
 	///////////////////////////////////
 	// Initialization function
@@ -29,19 +65,58 @@
 	}
 
 	///////////////////////////////////
-	// Mouse movement
+	// Mouse usage
 	///////////////////////////////////
-	function mouse(x, y)
+	function mouseMove(x, y)
 	{
-		// Make all objects white
-		for(var i = 0; i < data.scene.children.length; ++i)
-			data.scene.children[i].material.color.set(0xffffff);
+		if(data.dragging)
+		{
+			// Get position delta and update stored intersection
+			data.raycaster.setFromCamera(new THREE.Vector2(x, y), data.camera);
 
-		// Cast a ray for mouse picking and make the found object green
-		data.raycaster.setFromCamera(new THREE.Vector2(x, y), data.camera);
+			var point = data.raycaster.ray.origin.clone();
+			point.add(data.raycaster.ray.direction.clone().multiplyScalar(data.intersect.distance));
 
-		var objs = data.raycaster.intersectObjects(data.scene.children);
-		if(objs.length > 0) objs[0].object.material.color.set(0x00ff00);
+			var diff = point.clone().sub(data.intersect.point);
+			data.intersect.point = point;
+
+			// Move all vertices
+			for(var i = 0; i < data.intersect.object.geometry.vertices.length; ++i)
+				data.intersect.object.geometry.vertices[i].add(diff);
+
+			data.intersect.object.geometry.verticesNeedUpdate = true;
+			data.intersect.object.geometry.computeBoundingSphere();
+		}
+		else
+		{
+			// Make all objects white
+			for(var i = 0; i < data.scene.children.length; ++i)
+				data.scene.children[i].material.color.set(0xffffff);
+
+			// Cast a ray for mouse picking and make the found object green
+			data.raycaster.setFromCamera(new THREE.Vector2(x, y), data.camera);
+			var inters = data.raycaster.intersectObjects(data.scene.children);
+
+			if(inters.length < 1)
+				data.intersect = undefined;
+			else
+			{
+				// Also store the intersection
+				inters[0].object.material.color.set(0x00ff00);
+				data.intersect = inters[0];
+			}
+		}
+	}
+
+	function mouseDown(x, y)
+	{
+		if(data.intersect !== undefined)
+			data.dragging = true;
+	}
+
+	function mouseUp(x, y)
+	{
+		data.dragging = false;
 	}
 
 	///////////////////////////////////
@@ -72,7 +147,17 @@
 
 		window.addEventListener('mousemove', function(e)
 		{
-			mouse(e.clientX / window.innerWidth * 2 - 1, e.clientY / window.innerHeight * -2 + 1);
+			mouseMove(e.clientX / window.innerWidth * 2 - 1, e.clientY / window.innerHeight * -2 + 1);
+		}, false);
+
+		window.addEventListener('mousedown', function(e)
+		{
+			mouseDown(e.clientX / window.innerWidth * 2 - 1, e.clientY / window.innerHeight * -2 + 1);
+		}, false);
+
+		window.addEventListener('mouseup', function(e)
+		{
+			mouseUp(e.clientX / window.innerWidth * 2 - 1, e.clientY / window.innerHeight * -2 + 1);
 		}, false);
 
 		// Main renderer loop
