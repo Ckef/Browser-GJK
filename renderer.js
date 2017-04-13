a53398057d5c438dd38c8ca006bb8711795a2596
Stef Velzel <stefvelzel@hotmail.com>
2017-04-13 03:41:51 +0200

    Working GJK and some comments

--- renderer.js
+++ renderer.js
@@ -1,6 +1,6 @@
 (function()
 {
-	var MAX_ITERATIONS = 50;
+	var MAX_ITERATIONS = 64;
 
 	var data = // Data holder
 	{
@@ -16,44 +16,61 @@
 	///////////////////////////////////
 	function GJK(shape1, shape2)
 	{
-		/*var simplex = [new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3()];
+		// Keep track of how many vertices of the simplex are known
+		var simplex = [new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3()];
 		var n = 2;
 
 		// Use some arbitrary initial direction
 		var d = new THREE.Vector3(1, 0, 0);
 		simplex[1] = support(shape1, shape2, d);
 
+		// If no points are beyond the origin, the origin is outside the minkowski sum
+		// No collision is possible
 		if(simplex[1].dot(d) < 0)
 			 return false;
 
+		// Get another point in the opposite direction of the first
 		d = simplex[1].clone().negate();
 		simplex[0] = support(shape1, shape2, d);
 
+		// Same story as above
 		if(simplex[0].dot(d) < 0)
 			 return false;
 
+		// Pick a direction perpendiclar to the line
 		var tmp = simplex[1].clone().sub(simplex[0]);
 		var tmp2 = simplex[0].clone().negate();
 		d = tmp.clone().cross(tmp2).cross(tmp);
 
+		// We have two points, now we start iterating to get the simplex closer
+		// and closer to the origin of the minkowski sum
+		// Also we're dealing with floating point numbers and erros, so cap
+		// the maximum number of iterations to deal with weird cases
 		for(var i = 0; i < MAX_ITERATIONS; ++i)
 		{
 			var a = support(shape1, shape2, d);
 
+			// Dejavu
 			if(a.dot(d) < 0)
 				return false;
 
+			// We still only have a triangle
+			// Our goal is to find another point to get a tetrahedron that might
+			// enclose the origin
 			if(n == 2)
 			{
 				var aO = a.clone().negate();
+
+				// Edges we'll be testing and the triangle's normal
 				var ab = simplex[0].clone().sub(a);
 				var ac = simplex[1].clone().sub(a);
-
 				var abc = ab.clone().cross(ac);
+
 				var abp = ab.clone().cross(abc);
 
 				if(abp.dot(aO) > 0)
 				{
+					// Origin lies outside near edge ab
 					simplex[1] = simplex[0];
 					simplex[0] = a.clone();
 					d = ab.clone().cross(aO).cross(ab);
@@ -65,12 +82,15 @@
 
 				if(acp.dot(aO) > 0)
 				{
+					// Origin lies outside near edge ac
 					simplex[0] = a.clone();
 					d = ac.clone().cross(aO).cross(ac);
 
 					continue;
 				}
 
+				// At this point the origin must be within the triangle
+				// However we need to know if it is above or below
 				if(abc.dot(aO) > 0)
 				{
 					simplex[2] = simplex[1];
@@ -85,11 +105,12 @@
 					d = abc.clone().negate();
 				}
 
+				// We do however need a tetrahedron to eclose the origin
 				n = 3;
-
 				continue;
 			}
 
+			// By now we do have a tetrahedron, start checking if it contains the origin
 			var aO = a.clone().negate();
 			var ab = simplex[0].clone().sub(a);
 			var ac = simplex[1].clone().sub(a);
@@ -99,10 +120,13 @@
 			var acd = ac.clone().cross(ad);
 			var adb = ad.clone().cross(ab);
 
+			// Here come some generalized functions that are called by all cases
+			// Each case simply rotates the order of the vertices accordingly
 			var face = function()
 			{
 				if(ab.clone().cross(abc).dot(aO) > 0)
 				{
+					// In the region of AB
 					simplex[1] = simplex[0];
 					simplex[0] = a.clone();
 					d = ab.clone().cross(aO).cross(ab);
@@ -110,11 +134,11 @@
 				}
 				else
 				{
+					// In the region of ABC
 					simplex[2] = simplex[1];
 					simplex[1] = simplex[0];
 					simplex[0] = a.clone();
 					d = abc.clone();
-					n = 3;
 				}
 			}
 
@@ -122,6 +146,7 @@
 			{
 				if(abc.clone().cross(ac).dot(aO) > 0)
 				{
+					// In the region of AC
 					simplex[0] = a.clone();
 					d = ac.clone().cross(aO).cross(ac);
 					n = 2;
@@ -134,20 +159,24 @@
 			{
 				if(abc.clone().cross(ac).dot(aO) > 0)
 				{
+					// Origin is beyond AC from ABCs view
+					// Only need to test ACD
 					simplex[0] = simplex[1];
 					simplex[1] = simplex[2].clone();
 
 					ab = ac;
 					ac = ad.clone();
-
 					abc = acd.clone();
 
 					oneFace();
 				}
 
+				// At this point we're over ABC or over AB
+				// Revert back to a single face
 				else face();
 			}
 
+			// Check if the point is inside the tetrahedron
 			var ABC = 0x1;
 			var ACD = 0x2;
 			var ADB = 0x4;
@@ -156,17 +185,17 @@
 				(acd.dot(aO) > 0 ? ACD : 0) |
 				(adb.dot(aO) > 0 ? ADB : 0);
 
+			// Behind all three faces, collision!
 			if(tests == 0)
 				return true;
 
+			// Behind one face
 			if(tests == ABC)
-			{
 				oneFace();
-				continue;
-			}
 
-			if(tests == ACD)
+			else if(tests == ACD)
 			{
+				// Rotate ACD into ABC
 				simplex[0] = simplex[1];
 				simplex[1] = simplex[2].clone();
 
@@ -175,11 +204,11 @@
 				abc = acd.clone();
 
 				oneFace();
-				continue;
 			}
 
-			if(tests == ADB)
+			else if(tests == ADB)
 			{
+				// Rotate ADB into ABC
 				simplex[1] = simplex[0];
 				simplex[0] = simplex[2].clone();
 
@@ -188,17 +217,15 @@
 				abc = adb.clone();
 
 				oneFace();
-				continue;
 			}
 
-			if(tests == ABC | ACD)
-			{
+			// Behind two faces
+			else if(tests == ABC | ACD)
 				twoFaces();
-				continue;
-			}
 
-			if(tests == ACD | ADB)
+			else if(tests == ACD | ADB)
 			{
+				// Rotate ACD, ADB into ABC, ACD
 				tmp = simplex[0];
 				simplex[0] = simplex[1];
 				simplex[1] = simplex[2];
@@ -213,11 +240,11 @@
 				acd = adb.clone();
 
 				twoFaces();
-				continue;
 			}
 
-			if(tests ==  ADB | ABC)
+			else if(tests == ADB | ABC)
 			{
+				// Rotate ADB, ABC into ABC, ACD
 				tmp = simplex[1];
 				simplex[1] = simplex[0];
 				simplex[0] = simplex[2];
@@ -232,37 +259,42 @@
 				abc = adb.clone();
 
 				twoFaces();
-				continue;
 			}
 
-			// lol
-			return true;
+			// Well this shouldn't happen
+			// What the hell happened here
+			// Shitty floating point numbers being shitty I suppose
+			// Let's just say things collided
+			else return true;
 		}
 
-		return true;*/
+		// Out of iterations, but we're so damn close, let's just say it's a hit
+		return true;
 	}
 
 	function support(shape1, shape2, d) // vertices of shape 1 and 2 and direction
 	{
-		/*// Get some point on the minkowski sum (difference really)
+		// Get some point on the minkowski sum (difference really)
 		// Do this by getting the farthest point in d
 		var dir = d.clone().normalize();
 
 		var p1 = getFarthestPoint(shape1, dir);
 		var p2 = getFarthestPoint(shape2, dir.negate());
 
-		return p1.clone().sub(p2);*/
+		return p1.clone().sub(p2);
 	}
 
 	function getFarthestPoint(shape, d) // vertices of shape and direction (normalized)
 	{
-		/*// Project all vertices onto shape and get the longest
-		var p = new THREE.Vector3();
-		var l = 0;
+		var vertices = shape.geometry.vertices;
 
-		for(var i = 0; i < shape.geometry.vertices.length; ++i)
+		// Project all vertices onto shape and get the longest
+		var p = shape.localToWorld(vertices[0].clone());
+		var l = p.dot(d);
+
+		for(var i = 1; i < vertices.length; ++i)
 		{
-			var q = shape.localToWorld(shape.geometry.vertices[i].clone());
+			var q = shape.localToWorld(vertices[i].clone());
 			var proj = q.dot(d);
 
 			if(proj > l)
@@ -272,7 +304,7 @@
 			}
 		}
 
-		return p;*/
+		return p;
 	}
 
 	///////////////////////////////////
@@ -312,14 +344,12 @@
 
 				var shape1 = data.scene.children[i];
 				var shape2 = data.scene.children[j];
-				var col1 = shape1.material.color.getHex();
-				var col2 = shape2.material.color.getHex();
 
 				if(GJK(shape1, shape2))
 				{
-					if(col1 == 0xffffff)
+					if(shape1.material.color.getHex() == 0xffffff)
 						shape1.material.color.set(0xff0000);
-					if(col2 == 0xfffff)
+					if(shape2.material.color.getHex() == 0xffffff)
 						shape2.material.color.set(0xff0000);
 				}
 			}
@@ -349,18 +379,14 @@
 		else
 		{
 			// Make the green object white again
-			for(var i = 0; i < data.scene.children.length; ++i)
-				if(data.scene.children[i].material.color.getHex() == 0x00ff00)
-				{
-					data.scene.children[i].material.color.set(0xffffff);
-					break;
-				}
+			if(data.intersect !== undefined)
+				data.intersect.object.material.color.set(0xffffff);
 
 			// Cast a ray for mouse picking and make the found object green
 			data.raycaster.setFromCamera(new THREE.Vector2(x, y), data.camera);
 			var inters = data.raycaster.intersectObjects(data.scene.children, true);
 
-			if(inters.length < 1)
+			if(inters.length == 0)
 				data.intersect = undefined;
 			else
 			{
